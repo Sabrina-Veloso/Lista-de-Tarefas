61c902a737ee64cb3c5c34744175c51543b8e11b
Your Name <sabrinavelososilva@gmail.com>
2022-04-18 17:54:05 -0300

    comitei o projeto pronto

---
+++ lista.js
@@ -0,0 +1,46 @@
+const elementoLista = document.querySelector('ul');
+const elementoInput = document.querySelector('input');
+const elementoBotao = document.querySelector('button');
+
+const tarefas = []
+
+function mostraTarefas() {
+
+    elementoLista.innerHTML = ''
+
+    for (tarefa of tarefas) {
+        const elementoTarefa = document.createElement('li');
+        const textoTarefa = document.createTextNode(tarefa);
+
+        const elementoLink = document.createElement('a');
+        const pos = tarefas.indexOf(tarefa);
+
+        const linkText = document.createTextNode('X');
+        elementoLink.appendChild(linkText);
+
+        elementoLink.setAttribute('href', '#');
+        elementoLink.setAttribute('onclick', `deletaTarefa(${pos})`);
+
+        elementoTarefa.appendChild(textoTarefa);
+        elementoLista.appendChild(elementoTarefa);
+        elementoTarefa.appendChild(elementoLink);
+    }
+
+}
+mostraTarefas();
+
+function addTarefas() {
+
+    const textoTarefa = elementoInput.value;
+    tarefas.push(textoTarefa);
+    elementoInput.value = ''
+
+    mostraTarefas();
+}
+
+elementoBotao.setAttribute('onclick', 'addTarefas()');
+
+function deletaTarefa(pos) {
+    tarefas.splice(pos, 1);
+    mostraTarefas();
+}
